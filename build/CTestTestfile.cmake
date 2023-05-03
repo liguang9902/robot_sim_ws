0225756e3978511e0c55a4b239b3fbe3383c3fce
liguang9902 <liguang9902@qq.com>
2023-05-03 16:27:57 +0800

    仿真测试

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,6 +5,7 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("nav_demo")
 subdirs("gazebo")
 subdirs("401pius")
 subdirs("segway-401_plus")
